6f6286c19b838a687a5ba4292d265d58bc2e5fda
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-06-06 22:51:23 +0900

    update build configurations

--- config/DefaultFlags.cmake
+++ config/DefaultFlags.cmake
@@ -15,22 +15,32 @@ if(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
     "-std=f2018"
   )
 elseif(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
+  if(CMAKE_Fortran_COMPILER_VERSION VERSION_LESS 19.0)
+    set(STANDARD_FLAG_KEYWORD "f15")
+  else()
+    set(STANDARD_FLAG_KEYWORD "f18")
+  endif()
   set(
     CMAKE_Fortran_FLAGS_INIT
   )
-  set(
-    CMAKE_Fortran_FLAGS_RELEASE_INIT
-  )
   if(WIN32)
     set(
+      CMAKE_Fortran_FLAGS_RELEASE_INIT
+      "/stand:${STANDARD_FLAG_KEYWORD} /nologo /fpp /MP:3 /O2"
+    )
+    set(
       CMAKE_Fortran_FLAGS_DEBUG_INIT
-      "/stand:f18"
+      "/stand:${STANDARD_FLAG_KEYWORD} /nologo /fpp /MP:3"
       "/warn:declarations,general,usage,interfaces,unused"
     )
   else()
     set(
+      CMAKE_Fortran_FLAGS_RELEASE_INIT
+      "-stand ${STANDARD_FLAG_KEYWORD} -nologo -fpp -O2"
+    )
+    set(
       CMAKE_Fortran_FLAGS_DEBUG_INIT
-      "-stand f18"
+      "-stand ${STANDARD_FLAG_KEYWORD} -fpp"
       "-warn declarations,general,usage,interfaces,unused"
     )
   endif()
